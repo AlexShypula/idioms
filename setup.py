a11080399d11864b2dcbb887079950be6a517115
AlexShypula <shypula@seas.upenn.edu>
2026-01-30 04:02:04 +0000

    Update setup.py and add idioms/__init__.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="idioms",
     version="0.0.1",
-    packages=find_packages(),
+    package_dir={"idioms": "."},  # Map current directory as 'idioms' package
+    packages=["idioms", "idioms.idioms", "idioms.idioms.data", "idioms.idioms.data.idastubs"],
 )
